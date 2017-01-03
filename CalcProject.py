6a677c5ee209f6eda4343184234aa267380bd950
jksievers14 <jksievers14@gmail.com>
2017-01-03 14:49:48 -0500

    Update and rename README.md to CalcProject.py

---
+++ CalcProject.py
@@ -0,0 +1,22 @@
+# CalcProject
+print('This program calculates Farenheit to Celsius')
+
+f_string= input('Enter temperature in Farenheit:')
+
+f= float(f_string)
+
+c= (f-32) * 5 / 9
+
+print('The temperature in Celsius is:' + str(c) + 'degrees')
+
+print('This program calculates the area of a rectangle:')
+l_string= input('enter the length:')
+
+l= float(l_string)
+
+w_string= input('enter the width:')
+w=float(w_string)
+
+a= l*w
+print('tha area of the rectangle is:' + str(a) + "units")
+
